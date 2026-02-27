c28e8cdd55652bc5c098ffb2c5b93bded6c7c63f
jackkim523 <jackkim523@github.com>
2026-02-27 16:00:52 +0900

    Updates

--- _next/static/r6dTK4CtogGDr35qbqVYq/_buildManifest.js
+++ _next/static/r6dTK4CtogGDr35qbqVYq/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/nrf.notice//_next/:path+",
+        "destination": "/nrf.notice/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
